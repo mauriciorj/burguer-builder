71073b664053dbbd6aefba458c848171c96615ba
mauricio.o.mesquita@gmail.com <mauricio.o.mesquita@gmail.com>
2020-04-03 00:21:13 -0400

    Mobile styles is getting ready

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}> 
-            {props.children}
-        </main>
-    </Aux>
-);
+class layout extends Component {
+
+    state = {
+        SideDrawer: true
+    }
+
+    SideDrawerClosedHandler = () => {
+        this.setState({SideDrawer: false});
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer 
+                open={this.state.showSideDrawer} 
+                closed={this.SideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+
+};
 
 export default layout;
